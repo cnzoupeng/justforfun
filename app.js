1f8fa46fdaa8ecb990e923edfb687b54fcf0f4c5
ZouSir <zou.peng.cn@gmail.com>
2016-12-14 21:10:21 +0800

    x

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+"use strict"
 
-var routes = require('./api/index');
-var app = express();
+let up = require('./lib/update');
+let calc = require('./lib/calc')
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/', routes);
-app.use(function(req, res, next) {
-  res.status(404).json({code: 404, msg: 'Not found'});
+/*
+up.run(function(){
+  console.log('done')
 });
+*/
 
-app.use(function(err, req, res, next) {
-  console.log(err.stack);
-  res.status(500).json({code: 404, msg: err.message});
-});
+var arr = [
+  ['03', '05', '15', '16', '27', '30'],
+  ['02', '09', '15', '16', '27', '30'],
+  ['03', '08', '16', '17', '27', '30'],
+  ['04', '05', '15', '16', '22', '30'],
+  ['02', '05', '15', '16', '23', '29']
+]
 
-module.exports = app;
+calc.notInhist(arr);
